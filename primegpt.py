54313a53e96edb7c4e962b3b4a1609db25b0d696
poojaarabati <poojaarabati@gmail.com>
2025-05-01 21:46:02 -0700

    Add files via upload

---
+++ primegpt.py
@@ -0,0 +1,13 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/RISHI/Documents/pooja18/primegpt.py
+Enter start of interval: 14
+Enter end of interval: 25
+Prime numbers between 14 and 25 are:
+17 19 23 Traceback (most recent call last):
+  File "C:/Users/RISHI/Documents/pooja18/primegpt.py", line 14, in <module>
+    print_primes_in_interval(start, end)
+  File "C:/Users/RISHI/Documents/pooja18/primegpt.py", line 11, in print_primes_in_interval
+    prit()
+NameError: name 'prit' is not defined. Did you mean: 'print'?
